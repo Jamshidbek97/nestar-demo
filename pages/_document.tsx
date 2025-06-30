6cd07b6d71a303c2b539bba02e8a7e7b6f5bb2f3
Jamshidbek97 <urologiya@internet.ru>
2025-07-01 02:06:17 +0900

    Feat: Mui Customized integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,19 @@ export default function Document() {
         <meta name="title" content="Nestar Demo" />
         <meta name="robots" content="index,follow" />
         <link rel="icon" href="/img/logo/favicon.svg" type="img/png" />
+
+        <meta
+          name="keywords"
+          content="Nestar, Nestar.uz Jamshid97, Jamshid's Website"
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell anytime and anywhere in South Korea. |" +
+            "한국에서 언제 어디서나 사고팔 수 있습니다." +
+            "Janubiy Koreyada istalgan vaqtda va istalgan joyda sotib oling va soting."
+          }
+        />
       </Head>
       <body>
         <Main />
