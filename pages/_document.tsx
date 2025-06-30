79decf2109e253a7cbf4e08f30aa0b9a317cb462
Jamshidbek97 <urologiya@internet.ru>
2025-07-01 01:38:25 +0900

    Feat: Meta tags integration on documents

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" href="/img/logo/favicon.svg" type="img/png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
