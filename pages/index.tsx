ba5dd9cd8f38b973be7abe2157c31d6a5c7e4762
Jamshidbek97 <urologiya@internet.ru>
2025-07-03 00:28:21 +0900

    Feat: Creating HomePage related layout and css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Box className="container">Popular properties</Box>
+    <Stack className="home-page">
+      <Stack>
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+      <Stack>
         <Stack className="container">Top Agents</Stack>
-        <Box>Top properties</Box>
-        <Container>Events</Container>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
       </Stack>
     </Stack>
   );
