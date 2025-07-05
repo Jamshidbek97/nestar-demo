81e73bc67e852ed1569c0e5a0b2eef8594d3f536
Jamshidbek97 <urologiya@internet.ru>
2025-07-06 05:38:42 +0900

    Fix: Updated property page and create property card

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,13 +1,37 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import { NextPage } from "next";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import Filter from "@/libs/components/property/Filter";
+import { useState } from "react";
+import PropertyCard from "@/libs/components/property/PropertyCard";
 
 const PropertyList: NextPage = () => {
   console.log("Property component - pages router");
 
   return (
-    <div style={{ margin: "20px 0" }}>
-      <Stack className="container">Property list</Stack>
+    <div id="property-list-page">
+      <Stack className="container">
+        <Box className="right">
+          <span className="sort-label">Sort by</span>
+          <Button
+            className="sort-button"
+            endIcon={<KeyboardArrowDownRoundedIcon />}
+          >
+            New
+          </Button>
+        </Box>
+        <Stack className="property-page">
+          <Stack className="filter-config">
+            <Filter />
+          </Stack>
+          <Stack className="main-config">
+            {[1, 2, 3, 4, 5, 6].map((num) => (
+              <PropertyCard key={num} />
+            ))}
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 };
