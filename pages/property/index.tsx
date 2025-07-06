8faeb2909329a06a5d01ad1aefd056056b7e44f4
Jamshidbek97 <urologiya@internet.ru>
2025-07-07 01:41:23 +0900

    Fix: Adding pagination to Property page

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,5 +1,5 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Button, Pagination, Stack } from "@mui/material";
 import { NextPage } from "next";
 import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
 import Filter from "@/libs/components/property/Filter";
@@ -7,10 +7,10 @@ import { useState } from "react";
 import PropertyCard from "@/libs/components/property/PropertyCard";
 
 const PropertyList: NextPage = () => {
-  console.log("Property component - pages router");
+  const [page, setPage] = useState(1);
 
   return (
-    <div id="property-list-page">
+    <div id="property-list-page" style={{ marginBottom: "50px" }}>
       <Stack className="container">
         <Box className="right">
           <span className="sort-label">Sort by</span>
@@ -21,15 +21,23 @@ const PropertyList: NextPage = () => {
             New
           </Button>
         </Box>
+
         <Stack className="property-page">
-          <Stack className="filter-config">
+          <Box className="filter-config">
             <Filter />
-          </Stack>
-          <Stack className="main-config">
-            {[1, 2, 3, 4, 5, 6].map((num) => (
-              <PropertyCard key={num} />
-            ))}
-          </Stack>
+          </Box>
+
+          <Box className="card-wrap">
+            <Box className="main-config">
+              {[1, 2, 3, 4, 5, 6].map((i) => (
+                <PropertyCard key={i} />
+              ))}
+            </Box>
+
+            <Box className="pagination-container">
+              <Pagination count={10} page={1} color="primary" />
+            </Box>
+          </Box>
         </Stack>
       </Stack>
     </div>
